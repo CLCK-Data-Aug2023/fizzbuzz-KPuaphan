b58e130f6650a0bb42da60c44654bf5e967d66c0
Kevin Puaphan <kevin.puaphan@gmail.com>
2023-11-07 11:31:53 -0500

    Revised fizzbuzz challenge

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
 # add your code here
 
+for n in range(0,101):
+    if n % 5 == 0 and n % 3 ==0:
+        print("fizzbuzz")
+
+    elif n % 3 == 0:
+        print("fizz")
+
+    elif n % 5 == 0:
+        print("buzz")
+
+    else:
+        print(n)
